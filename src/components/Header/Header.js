03eaae433d8dc6f85978317f9db331952db04ea7
Borja <borja.navarro.web@gmail.com>
2020-03-10 08:35:22 +0100

    changed header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
-import './styles.scss';
+import icon from '../../img/icon.svg';
 
 function Header() {
   
   return (
     <header>
-
+      <Container>
+        <Row>
+          <Col>
+            <div className="img-container">
+              <img src={icon} alt=""/>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </header>
   );
 }
